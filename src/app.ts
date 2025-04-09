e8fd5581d788732ce26f2f6fed33b0c803cf79a0
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-04-09 17:45:14 +0600

    add app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { Server } from "http";
 import { StatusCodes } from "http-status-codes";
+import mongoose from "mongoose";
+
+import config from "./config";
 import { globalErrorHandler } from "./modules/middlewares/gloabalErrorHandler";
 import router from "./routes";
 
@@ -13,10 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://front end live link .vercel.app",
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -37,4 +38,31 @@ app.use("*", (req: Request, res: Response) => {
     message: "Route not found",
   });
 });
-export default app;
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_uri as string);
+    server = app.listen(config.port, () => {
+      console.log(`server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
+
+process.on("unhandledRejection", () => {
+  console.log("unhandledRejection is detected, shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+
+process.on("uncaughtException", () => {
+  console.log("uncaughtException is detected, shutting down...");
+  process.exit(1);
+});
